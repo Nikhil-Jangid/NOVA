598a82f9d369caf356893de1e7f3e9452855874f
Nikhilesh Jangid <nikhiljangid962@gmail.com>
2024-05-10 23:17:58 +0530

    Added MacOS Support for Speak function and updated requirements.

--- Body/speak.py
+++ Body/speak.py
@@ -1,13 +1,30 @@
 import pyttsx3
+import platform
+import os
 
-def Speak(Text):
-    engine = pyttsx3.init ("sapi5")
-    voices = engine.getProperty('voices')
-    engine.setProperty ('voices' ,voices[1].id)
-    engine.setProperty('rate' ,170)
-    print()
-    print(f"NOVA : {Text}.")
-    print()
-    engine.say(Text)
-    #engine.save_to_file("")
-    engine.runAndWait()
+system_platform = platform.system()
+
+def Speak(Text, lang='en'):
+    
+    if system_platform == 'Windows':
+        engine = pyttsx3.init ("sapi5")
+        voices = engine.getProperty('voices')
+        engine.setProperty ('voices' ,voices[1].id)
+        engine.setProperty('rate' ,170)
+        print()
+        print(f"NOVA : {Text}.")
+        print()
+        engine.say(Text)
+        #engine.save_to_file("")
+        engine.runAndWait()
+    elif system_platform == 'Darwin':  # macOS
+        print()
+        print(f"NOVA : {Text}.")
+        print()
+        cmd = f'say "{Text}"'
+        os.system(cmd)
+    else:
+        print("Text-to-speech is not supported on this platform.")
+
+
+Speak("Hello, I am Nova. How can I help you?")
